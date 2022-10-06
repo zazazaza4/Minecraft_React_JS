d8a735f6d369aaf4d01e0335450344a11cb00615
Yaroslav Osmukha <6715414@gmail.com>
2022-10-06 13:30:57 +0300

    add: useKeybord hook

---
+++ src/components/Player.jsx
@@ -0,0 +1,35 @@
+import { useSphere } from '@react-three/cannon';
+import { useFrame, useThree } from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import { Vector3 } from 'three';
+import { useKeyboard } from '../hooks/useKeyboard';
+
+export const Player = () => {
+  const actions = useKeyboard();
+  console.log('actions' + Object.entries(actions).filter(([k, v]) => v));
+
+  const { camera } = useThree();
+  const [ref, api] = useSphere(() => ({
+    mass: 1,
+    type: 'Dynamic',
+    position: [0, 1, 0]
+  }));
+
+  const vel = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.velocity.subscribe((v) => (vel.current = v));
+  }, [api.velocity]);
+
+  const pos = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.position.subscribe((p) => (pos.current = p));
+  }, [api.position]);
+
+  useFrame(() => {
+    camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
+
+    api.velocity.set(0, 0, 0);
+  });
+
+  return <mesh ref={ref}></mesh>;
+};
